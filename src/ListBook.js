5c6fca4fa2dc619f7b6c892eda5bfd5e3a128c06
vincentmhhon <vincentmhhon@gmail.com>
2017-08-02 22:35:07 -0400

    feat: decoupled into React components
    Added Book, BookShelfChanger, ListBook, and SearchBook components

---
+++ src/ListBook.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import Book from './Book'
+
+class ListBook extends Component {  
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.props.onOpenSearch()}>Add a book</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBook
